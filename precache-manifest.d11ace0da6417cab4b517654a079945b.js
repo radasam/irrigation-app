110d5ae61cb5fd5dc4a8c687f78fcc71c5be1138
radsam <sam.radage@gmail.com>
2020-03-06 12:43:22 +0000

    Updates

---
+++ precache-manifest.d11ace0da6417cab4b517654a079945b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307a24091de2f3efd927ecb80e188ed8",
+    "url": "/irrigation-app/index.html"
+  },
+  {
+    "revision": "572584c9d517f4125ee9",
+    "url": "/irrigation-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6e981bfbd74cee318133",
+    "url": "/irrigation-app/static/css/main.457d937f.chunk.css"
+  },
+  {
+    "revision": "572584c9d517f4125ee9",
+    "url": "/irrigation-app/static/js/2.9aaf3f70.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/irrigation-app/static/js/2.9aaf3f70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e981bfbd74cee318133",
+    "url": "/irrigation-app/static/js/main.b3b0892b.chunk.js"
+  },
+  {
+    "revision": "2e1c5f6208aefc8879cf",
+    "url": "/irrigation-app/static/js/runtime-main.b12bd637.js"
+  }
+]);
